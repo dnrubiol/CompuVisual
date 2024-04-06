e8c8d422d1261813a54b776b94496ca4b59b0ff4
unknown <decheverri@unal.edu.co>
2024-04-05 23:56:13 -0500

    basic levels

--- T1_Arcade/sketch.js
+++ T1_Arcade/sketch.js
@@ -1,20 +1,12 @@
-let scl = 5, cols, rows, player, frame, aliens = [], lasers = [], bunkers = [], alienDir, puntaje;
+let scl = 5, cols, rows, player, frame, aliens = [], lasers = [], bunkers = [], alienDir, alienSpeed, puntaje, level;
 
 function setup() {
     createCanvas(800, 800);
     frame = 0;
     cols = width / scl;
     rows = height / scl;
-    player = new Player(cols/2,rows-10);
-    for (let i = 0; i < 8; i++) {
-        aliens.push(new BackEnemy(i*16 + 2,20));
-        aliens.push(new FrontEnemy(i*16,30));
-    }
-    for (let i = 0; i < 4; i++) {
-        bunkers.push(new Bunker(i*cols/4+cols/8-8,rows - 30));
-    }
-    alienDir = createVector(2, 0);
-    puntaje = 0;
+    setupLevel1();
+    alienDir = createVector(alienSpeed, 0);
 }
 
 function draw() {
@@ -29,14 +21,14 @@ function draw() {
     for (const enemy of aliens) {
         if (frame % 60 == 0) {
             enemy.setDir(dirX,dirY);
-            if (dirX === 2 && enemy.position.x + 10 >= cols) {
-                alienDir = createVector(0, 2);
-            } else if (dirY === 2 && enemy.position.x + 10 >= cols) {
-                alienDir = createVector(-2, 0);
-            } else if (dirX === -2 && enemy.position.x <= 0) {
-                alienDir = createVector(0, 2);
-            } else if (dirY === 2 && enemy.position.x <= 0) {
-                alienDir = createVector(2, 0);
+            if (dirX === alienSpeed && enemy.position.x + 10 >= cols) {
+                alienDir = createVector(0, alienSpeed);
+            } else if (dirY === alienSpeed && enemy.position.x + 10 >= cols) {
+                alienDir = createVector(-alienSpeed, 0);
+            } else if (dirX === -alienSpeed && enemy.position.x <= 0) {
+                alienDir = createVector(0, alienSpeed);
+            } else if (dirY === alienSpeed && enemy.position.x <= 0) {
+                alienDir = createVector(alienSpeed, 0);
             }
         }
         enemy.render();
@@ -70,6 +62,16 @@ function draw() {
         if (lasers[i].toDelete === true) lasers.splice(i,1);
     }
 
+    if (aliens.length === 0) {
+        if (level === 1) {
+            setupLevel2();
+        } else if (level === 2) {
+            setupLevel3();
+        } else if (level === 3) {
+            setupLevel4();
+        }
+    }
+
     frame++;
 }
 
@@ -90,4 +92,83 @@ function keyPressed() {
       let laser = new Laser(player.position.x + 3, player.position.y - 1);
       lasers.push(laser);
     }
+}
+
+function setupLevel1() {
+    level = 1;
+    player = new Player(cols/2,rows-10);
+    aliens = []; 
+    lasers = []; 
+    bunkers = [];
+    for (let i = 0; i < 6; i++) {
+        aliens.push(new BackEnemy(i*21 + 2,20));
+        aliens.push(new FrontEnemy(i*21,30));
+    }
+    for (let i = 0; i < 4; i++) {
+        bunkers.push(new Bunker(i*cols/4+cols/8-8,rows - 30));
+    }
+    alienSpeed = 2;
+    alienDir = createVector(alienSpeed, 0);
+    puntaje = 0;
+}
+
+function setupLevel2() {
+    level = 2;
+    player = new Player(cols/2,rows-10);
+    aliens = []; 
+    lasers = []; 
+    bunkers = [];
+    for (let i = 0; i < 6; i++) {
+        aliens.push(new BackEnemy(i*21 + 2,10));
+        aliens.push(new BackEnemy(i*21 + 2,20));
+        aliens.push(new FrontEnemy(i*21,30));
+    }
+    for (let i = 0; i < 4; i++) {
+        bunkers.push(new Bunker(i*cols/4+cols/8-8,rows - 30));
+    }
+    alienSpeed = 4;
+    alienDir = createVector(alienSpeed, 0);
+    puntaje = 0;
+}
+
+function setupLevel3() {
+    level = 3;
+    player = new Player(cols/2,rows-10);
+    aliens = []; 
+    lasers = []; 
+    bunkers = [];
+    for (let i = 0; i < 8; i++) {
+        aliens.push(new BackEnemy(i*16 + 2,10));
+        aliens.push(new BackEnemy(i*16 + 2,20));
+        aliens.push(new FrontEnemy(i*16,30));
+        aliens.push(new FrontEnemy(i*16,40));
+    }
+    for (let i = 0; i < 3; i++) {
+        bunkers.push(new Bunker(i*cols/3+cols/6-8,rows - 30));
+    }
+    alienSpeed = 6;
+    alienDir = createVector(alienSpeed, 0);
+    puntaje = 0;
+}
+
+function setupLevel4() {
+    level = 4;
+    player = new Player(cols/2,rows-10);
+    aliens = []; 
+    lasers = []; 
+    bunkers = [];
+    for (let i = 0; i < 8; i++) {
+        aliens.push(new BackEnemy(i*16 + 2,-10));
+        aliens.push(new BackEnemy(i*16 + 2,0));
+        aliens.push(new BackEnemy(i*16 + 2,10));
+        aliens.push(new BackEnemy(i*16 + 2,20));
+        aliens.push(new FrontEnemy(i*16,30));
+        aliens.push(new FrontEnemy(i*16,40));
+    }
+    for (let i = 0; i < 3; i++) {
+        bunkers.push(new Bunker(i*cols/3+cols/6-8,rows - 30));
+    }
+    alienSpeed = 8;
+    alienDir = createVector(alienSpeed, 0);
+    puntaje = 0;
 }
