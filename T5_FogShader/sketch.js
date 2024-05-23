fc8da7934a6bd1c790df6eb2c25a5ca687fe6325
dnrubiol <dnrubiol@unal.edu.co>
2024-05-23 16:31:05 -0500

    giros cubos

--- T5_FogShader/sketch.js
+++ T5_FogShader/sketch.js
@@ -1,6 +1,6 @@
 // Variables para los cubos
-let numCubes = 15;
-let cubeSize = 40;
+let numCubes = 100;
+let cubeSize = 50;
 let cubes = [];
 
 // Variables para el shader de niebla
@@ -11,6 +11,12 @@ let fogFar = 200.0;
 let angleX = 0;
 let angleY = 0;
 
+// Variables globales para la rotación
+let rotationAngleX = 0;
+let rotationAngleY = 0;
+let rotationAngleZ = 0;
+let rotationSpeedX, rotationSpeedY, rotationSpeedZ;
+
 function preload() {
   // Cargar el shader
   fogShader = loadShader('fog.vert', 'fog.frag');
@@ -19,25 +25,38 @@ function preload() {
 function setup() {
   createCanvas(800, 400, WEBGL);
 
-  // Crear los cubos
+  // Asignar velocidades de rotación aleatorias para todos los cubos
+  rotationSpeedX = 0.008;
+  rotationSpeedY = 0.008;
+  rotationSpeedZ = 0.008;
+
+  // Configurar la perspectiva de la cámara
+  let fov = 0.8;  // Ajustar el FOV para hacer zoom
+  let aspect = width / height;
+  let near = 1;
+  let far = 10000;
+  perspective(fov, aspect, near, far);
+
+  // Crear los cubos con un espacio entre ellos
   for (let i = 0; i < numCubes; i++) {
-    cubes.push(new Cube(i * (cubeSize + 10)));
+    cubes.push(new Cube(i * ((cubeSize + 2) * 2))); // Espacio entre cubos
   }
 }
 
 function draw() {
   background(200);
 
-   // Rotar la cámara
-   //let camX = map(0, 0, width, -200, 200);
-   let camX = -500;
-   //let camY = map(0, 0, height, -200, 200);
-   let camY = -100; 
-   camera(camX, camY, (height/2) / tan(PI/6), 0, 0, 0, 0, 1, 0);
-   
-   // Rotación de la escena
-   rotateX(angleX);
-   rotateY(angleY);
+  // Rotar la cámara
+  camera(-60, 0, 150, 0, 0, 120, 0, 1, 0); // Mover la cámara más cerca
+
+  // Incrementar los ángulos de rotación globales
+  rotationAngleX += rotationSpeedX;
+  rotationAngleY += rotationSpeedY;
+  rotationAngleZ += rotationSpeedZ;
+
+  // Rotación de la escena
+  rotateX(angleX);
+  rotateY(angleY);
 
   // Usar el shader de niebla
   shader(fogShader);
@@ -45,20 +64,25 @@ function draw() {
   fogShader.setUniform('fogFar', fogFar);
 
   // Dibujar los cubos
-  for (let i = 0; i < cubes.length; i++) {
-    cubes[i].display();
+  for (let cube of cubes) {
+    cube.display();
   }
 }
 
 class Cube {
   constructor(x) {
-    this.x = x;
+    this.position = createVector(x, 0, 0);
+    this.size = 58;
   }
 
   display() {
     push();
-    translate(this.x - width / 2 + cubeSize / 2, 0, 0);
-    box(cubeSize);
+    translate(this.position.x, this.position.y, this.position.z);
+    // Aplicar rotación global
+    rotateX(rotationAngleX);
+    rotateY(rotationAngleY);
+    rotateZ(rotationAngleZ);
+    box(this.size);
     pop();
   }
 }
@@ -66,11 +90,5 @@ class Cube {
 // Función para manejar el cambio en los sliders de fogNear y fogFar
 function updateFog() {
   fogNear = map(document.getElementById('fogNearSlider').value, 0, 100, 0, 200);
-  fogFar = map(document.getElementById('fogFarSlider').value, 0, 100, 0, 500);
+  fogFar = map(document.getElementById('fogFarSlider').value, 0, 100, 0, 1000);
 }
-
-// function mouseDragged() {
-//   // Incrementar los ángulos de rotación basados en el movimiento del mouse
-//   angleX += (mouseY - pmouseY) * 0.01;
-//   angleY += (mouseX - pmouseX) * 0.01;
-// }
